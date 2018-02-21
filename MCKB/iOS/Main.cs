5fceb6c7bb5e2f7bfc145e7ff4aba217b56976de
Maximilian Hans Peter Thomas Peßl <maximilian.pessl@gmail.com>
2018-02-21 22:44:45 +0100

    TEST Commit

--- MCKB/iOS/Main.cs
+++ MCKB/iOS/Main.cs
@@ -12,6 +12,7 @@ namespace MCKB.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            //TEST
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
