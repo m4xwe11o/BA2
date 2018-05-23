b57018a175bc28df0ef7c8ae5324d8d239493120
Maximilian Hans Peter Thomas Peßl <maximilian.pessl@gmail.com>
2018-05-23 23:05:23 +0200

    project cleanup and finished 2 chap

---
+++ MCKB/MCKB/Article.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace MCKB
+{
+    public class Article
+    {
+        public string ArticleName { get; set; }
+        public Article()
+        {
+        }
+
+    }
+
+    ObservableCollection<Article> articles = new ObservableCollection<Article>();
+    public ArticleListPage()
+    {
+        //defined in XAML to follow
+        ArticleView.ItemsSource = articles;
+    }
+    public ArticleListPage()
+    {
+        articles.Add(new Article { ArticleName = "Rob Finnerty" });
+        articles.Add(new Article { ArticleName = "Bill Wrestler" });
+        articles.Add(new Article { ArticleName = "Dr. Geri-Beth Hooper" });
+    }
+}
